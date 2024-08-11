6d2831bf2e7b17b0be3e3e4ba7a3b7a41be05508
Anthony <semprini.anthony@gmail.com>
2024-08-12 01:28:19 +0200

    operation en python

--- hello.py
+++ hello.py
@@ -11,3 +11,20 @@ taille = 1.80
 print("Mon nom est :", nom)
 print("Mon age est :", age)
 print("Ma taille est :", taille)
+
+# operation arithmetique
+
+a = 15
+b = 5
+
+add = a+b
+sous = a-b
+div = a/b
+multi = a*b
+
+print("add result : ",add)
+print("sous result : ",sous)
+print("div result : ",div)
+print("multi result : ",multi)
+
+
