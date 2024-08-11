e366646fd117e7130704975b7145b404111666df
Anthony <semprini.anthony@gmail.com>
2024-08-12 01:23:13 +0200

    first commit

---
+++ hello.py
@@ -0,0 +1,13 @@
+print("hello world!")
+
+# Variables et types
+
+nom = "Tony"
+age = "32"
+taille = 1.80
+
+# afficher variables
+
+print("Mon nom est :", nom)
+print("Mon age est :", age)
+print("Ma taille est :", taille)
